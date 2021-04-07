146f5cdcc56c14289341588ed45d9d7c5468209a
Giorgio Comai <g@giorgiocomai.eu>
2021-04-07 21:48:33 +0200

    remove test depending on order of other tests being run

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -1,10 +1,3 @@
-test_that("cache defaults to FALSE", {
-  expect_equal(
-    object = tw_check_cache(cache = NULL),
-    expected = FALSE
-  )
-})
-
 test_that("cache respects param when given", {
   expect_equal(
     object = tw_check_cache(cache = TRUE),
