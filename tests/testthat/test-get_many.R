d4004cd92f027a3daaeddc0d38c75dc1079a211b
Giorgio Comai <g@giorgiocomai.eu>
2021-08-03 11:46:19 +0200

    more robust test for tw_get_label()

--- tests/testthat/test-get_many.R
+++ tests/testthat/test-get_many.R
@@ -1,18 +1,24 @@
 library("testthat")
 
-test_that("check if labels are returned correctly when more than one id given", {
+test_that("check if labels are returned correctly when more than one id given, including NA", {
   testthat::skip_if_offline()
 
   expect_equal(object = {
     tw_get_label(
       id = c(
         "Q180099",
-        "Q228822"
+        "Q228822",
+        "Not an id",
+        "Q180099",
+        NA
       ),
       language = "en"
     )
   }, expected = c(
     "Margaret Mead",
-    "Ruth Benedict"
+    "Ruth Benedict",
+    NA,
+    "Margaret Mead",
+    NA
   ))
 })
