a941bceff1dc43102ffcb5e02439da9af9c80f67
Giorgio Comai <g@giorgiocomai.eu>
2022-01-21 20:23:59 +0100

    introduce tw_empty_item

---
+++ data-raw/tw_empty_item.R
@@ -0,0 +1,15 @@
+## code to prepare `tw_empty_item` dataset goes here
+library("dplyr", warn.conflicts = FALSE)
+
+tw_empty_item <- c(
+  "id",
+  "property",
+  "value",
+  "rank"
+) %>%
+  purrr::map_dfc(setNames,
+                 object = list(character())
+  )
+
+
+usethis::use_data(tw_empty_item, overwrite = TRUE)
