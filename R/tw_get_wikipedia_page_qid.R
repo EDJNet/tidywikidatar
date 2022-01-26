6603d8b533b10f27ca44a6af3287175669dfa446
Giorgio Comai <g@giorgiocomai.eu>
2022-01-26 16:42:00 +0100

    make tw_get_wikipedia_base_api_url() more generic

--- R/tw_get_wikipedia_page_qid.R
+++ R/tw_get_wikipedia_page_qid.R
@@ -5,17 +5,17 @@
 #' @param url A character vector with the full URL to one or more Wikipedia pages. If given, title and language can be left empty.
 #' @param title Title of a Wikipedia page or final parts of its url. If given, url can be left empty, but language must be provided.
 #' @param language Two-letter language code used to define the Wikipedia version to use. Defaults to language set with `tw_set_language()`; if not set, "en". If url given, this can be left empty.
+#' @param action Defaults to "query". Usually either "query" or "parse". In principle, any valid action value, see: https://www.mediawiki.org/w/api.php
 #'
 #' @return A character vector of base urls to be used with the MediaWiki API
 #' @export
 #'
 #' @examples
-#' if (interactive()) {
-#'   tw_get_wikipedia_base_api_url(title = "Margaret Mead", language = "en")
-#' }
+#' tw_get_wikipedia_base_api_url(title = "Margaret Mead", language = "en")
 tw_get_wikipedia_base_api_url <- function(url = NULL,
                                           title = NULL,
-                                          language = tidywikidatar::tw_get_language()) {
+                                          language = tidywikidatar::tw_get_language(),
+                                          action = "query") {
   if (is.null(url) == TRUE) {
     if (is.null(title) == TRUE) {
       usethis::ui_stop("Either url or title must be provided")
@@ -40,7 +40,9 @@ tw_get_wikipedia_base_api_url <- function(url = NULL,
   api_url <- stringr::str_c(
     "https://",
     language,
-    ".wikipedia.org/w/api.php?action=query&redirects=true&format=json&titles=",
+    ".wikipedia.org/w/api.php?action=",
+    action,
+    "&redirects=true&format=json&titles=",
     utils::URLencode(URL = title)
   )
 
