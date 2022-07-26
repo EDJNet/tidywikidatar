b89f78fcf1b78c15c08d61fd47254bd2093a51d9
Giorgio Comai <g@giorgiocomai.eu>
2022-07-26 10:43:12 +0200

    more consistent error messages in different use cases

--- R/tw_get_wikipedia_page_qid.R
+++ R/tw_get_wikipedia_page_qid.R
@@ -31,6 +31,15 @@ tw_get_wikipedia_base_api_url <- function(url = NULL,
       usethis::ui_stop("Either language or full url must be provided")
     }
   } else {
+    check_url_lv <- stringr::str_starts(string = url, pattern = "http", negate = TRUE)
+    if (sum(is.na(check_url_lv))>0) {
+      url <- url[is.na(check_url_lv)==FALSE]
+      usethis::ui_warn("One or more of the given URLs is actually NA. Only valid URLs will be processed.")
+    }
+    check_url_lv <- stringr::str_starts(string = url, pattern = "http", negate = TRUE)
+    if (sum(check_url_lv)!=length(check_url_lv)) {
+      usethis::ui_stop(x = "One or more of the Wikipedia URL provided does not start with `http` as expected for a URL. If you are actually providing Wikipedia page titles, leave the `url` parameter to NULL, and use the `title` parameter instead.")
+    }
     title <- stringr::str_extract(
       string = url,
       pattern = "(?<=https://[[a-z]][[a-z]].wikipedia.org/wiki/).*"
--- tests/testthat/test-tw_get_wikipedia_category_members.R
+++ tests/testthat/test-tw_get_wikipedia_category_members.R
@@ -24,7 +24,7 @@ if (interactive()) {
         type = "subcat"
       )
 
-      nrow(sub_category_members_df) > 0
+      nrow(sub_category_members_df) > 1
     })
   })
 
